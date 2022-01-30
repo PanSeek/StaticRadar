5c9d1efddf2e35954cb3487f2e71d303a1eb9441
PanSeek <35432222+PanSeek@users.noreply.github.com>
2022-01-30 20:58:33 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,15 +1,14 @@
-﻿#include <Windows.h>
+﻿#include <windows.h>
 
-class CMain {
-public:
-	CMain() {
+struct stMain {
+	stMain() {
 		*reinterpret_cast<float*>(0xBA8308) = 1.0f;
 		*reinterpret_cast<float*>(0xBA830C) = 0.0f;
 		VirtualProtect(reinterpret_cast<LPVOID>(0x586B04), 2, PAGE_READWRITE, &oldProt);
 		memset(reinterpret_cast<void*>(0x586B04), 0xEB, 0x2);
 		VirtualProtect(reinterpret_cast<LPVOID>(0x586B04), 2, oldProt, &oldProt);
 	};
-	~CMain() {
+	~stMain() {
 		VirtualProtect(reinterpret_cast<LPVOID>(0x586B04), 2, PAGE_READWRITE, &oldProt);
 		memcpy(reinterpret_cast<void*>(0x586B04), "\x68\xCB", 0x2); // E8 68 CB FF FF
 		VirtualProtect(reinterpret_cast<LPVOID>(0x586B04), 2, oldProt, &oldProt);
